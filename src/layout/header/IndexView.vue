103c780ddc258db7a41e01a6ca0f627a6ff9389d
耗子 <i@haozi.net>
2023-10-26 00:25:08 +0800

    feat: 添加重载页面

--- src/layout/header/IndexView.vue
+++ src/layout/header/IndexView.vue
@@ -4,6 +4,7 @@ import MenuCollapse from './components/MenuCollapse.vue'
 import FullScreen from './components/FullScreen.vue'
 import UserAvatar from './components/UserAvatar.vue'
 import ThemeMode from './components/ThemeMode.vue'
+import ReloadPage from '@/layout/header/components/ReloadPage.vue'
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import ThemeMode from './components/ThemeMode.vue'
     <BreadCrumb ml-15 hidden sm:block />
   </div>
   <div ml-auto flex items-center>
+    <ReloadPage />
     <ThemeMode />
     <FullScreen />
     <UserAvatar />
---
+++ src/layout/header/components/ReloadPage.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { router } from '@/router'
+import { useAppStore } from '@/store'
+
+const appStore = useAppStore()
+
+const handleReloadPage = () => {
+  appStore.reloadPage()
+}
+</script>
+
+<template>
+  <n-icon mr-20 cursor-pointer size="18" @click="handleReloadPage">
+    <icon-mdi-refresh />
+  </n-icon>
+</template>
