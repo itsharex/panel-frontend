455666bf9c75236793886e3202f26833f1fee979
耗子 <i@haozi.net>
2023-10-30 03:19:38 +0800

    feat: toolbox view

--- src/api/plugins/s3fs/index.ts
+++ src/api/plugins/s3fs/index.ts
@@ -2,7 +2,8 @@ import { request } from '@/utils'
 
 export default {
   // 列表
-  list: () => request.get('/plugins/s3fs/list'),
+  list: (page: number, limit: number) =>
+    request.get('/plugins/s3fs/list', { params: { page, limit } }),
   // 添加
   add: (data: any) => request.post('/plugins/s3fs/add', data),
   // 删除
