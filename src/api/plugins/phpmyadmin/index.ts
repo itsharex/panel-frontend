9091fc0dc5dd925dadf8a3c155aefd4c317303a4
耗子 <i@haozi.net>
2024-06-25 03:07:45 +0800

    feat: phpMyAdmin支持编辑配置

--- src/api/plugins/phpmyadmin/index.ts
+++ src/api/plugins/phpmyadmin/index.ts
@@ -6,5 +6,10 @@ export default {
   info: (): Promise<AxiosResponse<any>> => request.get('/plugins/phpmyadmin/info'),
   // 设置端口
   port: (port: number): Promise<AxiosResponse<any>> =>
-    request.post('/plugins/phpmyadmin/port', { port })
+    request.post('/plugins/phpmyadmin/port', { port }),
+  // 获取配置
+  getConfig: (): Promise<AxiosResponse<any>> => request.get('/plugins/phpmyadmin/config'),
+  // 保存配置
+  saveConfig: (config: string): Promise<AxiosResponse<any>> =>
+    request.post('/plugins/phpmyadmin/config', { config })
 }
