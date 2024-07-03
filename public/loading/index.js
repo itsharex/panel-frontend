936fd44567476e1bdbc0366eed825981d378630c
耗子 <i@haozi.net>
2024-07-04 02:38:40 +0800

    fix: color

--- public/loading/index.js
+++ public/loading/index.js
@@ -1,6 +1,6 @@
 function addThemeColorCssVars() {
   const key = '__THEME_COLOR__'
-  const defaultColor = '#316c72'
+  const defaultColor = '#66CCFF'
   const themeColor = window.localStorage.getItem(key) || defaultColor
   const cssVars = `--primary-color: ${themeColor}`
   document.documentElement.style.cssText = cssVars
